9964dd55b91f1410dde17f442096a5517a7da104
wb-yq488745 <wb-yq488745@alibaba-inc.com>
2019-06-27 20:20:05 +0800

    fix bugs

---
+++ app-new/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="layout-default">
+    <el-header>
+      <my-header />
+    </el-header>
+    <el-main>
+      <div>
+        <nuxt />
+      </div>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import MyHeader from "@/components/public/header/index.vue"
+export default {
+  components:{
+    MyHeader
+  }
+}
+</script>
