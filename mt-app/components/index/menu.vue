3887155db06a77a919b67b0a34dcca37e1841eaa
wb-yq488745 <wb-yq488745@alibaba-inc.com>
2019-06-29 18:25:02 +0800

    添加

--- mt-app/components/index/menu.vue
+++ mt-app/components/index/menu.vue
@@ -7,12 +7,10 @@
             </dd>
         </dl>
         <div class="detail">
-            <!-- <template v-for="(item,idx) in curdetail" >
-                <h4>标题</h4>
-                <span>子项</span>
-                <span>子项</span>
-                <span>子项</span>
-            </template> -->
+            <template v-for="(item,idx) in curdetail" >
+                <h4 :key="idx">{{item.title}}</h4>
+                <span v-for="v in item.child" :key="v" >{{v}}</span>
+            </template>
         </div>
     </div>
 </template>
@@ -24,29 +22,32 @@ export default {
             kind:'',
             menu:[{
                 type:'food',
-                name:'美食'
+                name:'美食',
+                child:[{
+                    title:'美食',
+                    child:['代金券', '甜点饮品', '火锅', '小吃快餐']
+                }]
             },{
                 type:'takeout',
-                name:'外卖'
+                name:'外卖',
+                child:[{
+                    title:'外卖',
+                    child:['美团外卖']
+                }]
             },
             {
                 type:'hotel',
-                name:'酒店'
-            },{
-                type:'takeout',
-                name:'外卖'
-            },{
-                type:'takeout',
-                name:'外卖'
-            },{
-                type:'takeout',
-                name:'外卖'
+                name:'酒店',
+                child:[{
+                    title:'酒店星级',
+                    child:['经济型','舒适/三星','高档/四星','豪华/五星']
+                }]
             }]
         }
     },
     computed: {
         curdetail:function(){
-            return []
+            return this.menu.filter((item) => {item.type === this.kind})[0]
         }
     },
 }
