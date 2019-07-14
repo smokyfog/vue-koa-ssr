28577c57a26d806925e54afbca9e9ffaf30fd236
smokyfog <1473092739@qq.com>
2019-07-15 00:02:50 +0800

    切换城市页面完成

--- mt-app/pages/changeCity.vue
+++ mt-app/pages/changeCity.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="page-changeCity">
     <el-row>
-      <el-col :span='24'>
+      <el-col :span="24">
         <iSelect/>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span='24'>
+      <el-col :span="24">
         <hot/>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span='24'>1</el-col>
+      <el-col :span="24">
+        <categroy/>
+      </el-col>
     </el-row>
   </div>
 </template>
+
 <script>
 import iSelect from '@/components/changeCity/iselect.vue'
 import Hot from '@/components/changeCity/hot.vue'
+import Categroy from '@/components/changeCity/categroy.vue'
 export default {
   components:{
     iSelect,
-    Hot
+    Hot,
+    Categroy
   }
 }
 </script>
 
 <style lang="css">
-
 </style>
